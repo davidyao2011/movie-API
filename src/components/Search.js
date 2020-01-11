f66ec6acb3ca0dc115b0cbb071246af8ef9312e5
David Yao <davidyao2011@hotmail.com>
2020-01-11 23:00:26 +1300

    first commit

---
+++ src/components/Search.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+
+const Search =(props) => {
+    const [searchValue, setSearchValue] = useState("");
+
+    const handleSearchInputChange =(event) => {
+        setSearchValue(event.target.value);
+    }
+
+    const resetInputField = () => {
+        setSearchValue("")
+    }
+
+    const callSearchFunction = (event) => {
+        event.preventDefault();
+        props.search(searchValue);
+        resetInputField();
+    }
+
+    return (
+        <form className='search'>
+            <input value={searchValue}
+            onChange={handleSearchInputChange}
+            type='text' placeholder='searh the movie' />
+
+            <input onClick={callSearchFunction} type='Search'/>
+
+        </form>
+    );
+}
+
+export default Search;
